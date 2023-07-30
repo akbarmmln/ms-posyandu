3582a32a57acd7f2da7545f22c7a1981c0db41f8
Muhammad Taufik <muhammad.taufik@adira.co.id>
2023-07-30 15:51:33 +0700

    update code

---
+++ api/v1/healthcek/controller.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const sequelize = require('../../../config/db').Sequelize;
+const logger = require('../../../config/logger');
+const errMsg = require('../../../error/resError');
+const rsMsg = require('../../../response/rs');
+let health = {serverOk: false,dbOk: false};
+
+exports.healtyCheck = async function(req, res){
+    health.serverOk = true;
+    let asynccc = await updateHealthResponse();
+    if(asynccc == 200){
+        return res.status(200).json(rsMsg());
+    }else{
+        return res.status(500).json(errMsg('01000'));
+    }
+}
+
+async function checkMySql() {
+    await sequelize.authenticate()
+    .then(() => {health.dbOk = true})
+    .catch(err => {health.dbOk = false});
+}
+
+async function updateHealthResponse() {
+    await checkMySql();
+    const isReady = health.serverOk && health.dbOk;
+
+    if (isReady) {
+        logger.debug('MSPOSYANDU_READINESS_SUCCESS] - Ready to serve traffic');
+        return 200;
+    } else {
+        logger.error('[MSPOSYANDU_READINESS_ERROR] - Unable to serve traffic');
+        return 500
+    }
+}
