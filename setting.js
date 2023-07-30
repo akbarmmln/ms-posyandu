2ad71c13dcdba70bd34dd02a9b93156f75a333c2
Muhammad Taufik <muhammad.taufik@adira.co.id>
2023-07-30 16:38:42 +0700

    update code

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
 module.exports = {
     mysql: {
-        hostname: "rm-d9jl3xtniu9x99a4f2o.mysql.ap-southeast-5.rds.aliyuncs.com",
-        username: "qcadmin",
-        password: "sB1w1^%1qoQ9HhHv",
-        port: "3306",
-        dbname: "ms_psynd",
+        hostname: process.env.MYSQL_HOSTNAME,
+        username: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_PASSWORD,
+        port: process.env.MYSQL_PORT,
+        dbname: process.env.MYSQL_DATABASE,
     },
 };
