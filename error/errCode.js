f78ec1fd882bb7e539440b30cfa8860b4c242273
Muhammad Taufik <muhammad.taufik@adira.co.id>
2023-08-01 14:40:09 +0700

    update code

--- error/errCode.js
+++ error/errCode.js
@@ -2,8 +2,7 @@ const errCode = {
     '10000': 'internal server error',
     '10001': 'nik not found',
     '10002': 'password not match',
-    '10003': 'nik already register',
-    '10004': 'something error',
+    '10003': 'nik already register'
   };
   
   module.exports = errCode;
